eb717e8d66957cf49c2ff7f68bc2708980f2b1b9
mfarid24si-afk <m.farid24si@mahasiswa.pcr.ac.id>
2025-10-16 11:18:52 +0700

    pertemuan6

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MahasiswaController;
 use App\Http\Controllers\QuestionController;
 use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\PelangganController;
+
 
 Route::get('/', function () {
     return view('welcome');
@@ -17,10 +19,10 @@ Route::get('/mahasiswa', function () {
 })->name('mahasiswa.show');
 
 Route::get('/nama/{param1}', function ($param1) {
-    return 'Nama saya: '.$param1;
+    return 'Nama saya: ' . $param1;
 });
 Route::get('/nim/{param1?}', function ($param1 = '') {
-    return 'NIM saya: '.$param1;
+    return 'NIM saya: ' . $param1;
 });
 
 Route::get('/mahasiswa/{param1}', [MahasiswaController::class, 'show']);
@@ -33,7 +35,9 @@ Route::get('/home', [HomeController::class, 'index'])
     ->name('home');
 
 Route::post('question/store', [QuestionController::class, 'store'])
-		->name('question.store');
+    ->name('question.store');
 
-        Route::get('dashboard', [DashboardController::class, 'index'])
-        ->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'index'])
+    ->name('dashboard');
+
+Route::resource('pelanggan', PelangganController::class);
